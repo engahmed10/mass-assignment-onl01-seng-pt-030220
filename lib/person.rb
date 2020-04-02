51c65d3aa5c6c8821fb6e980164e8bc400e6853a
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-02 06:10:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,12 @@
 class Person
   #your code here
   attr_accessor :name
-def initialize(attr)
-
-       attr.each do |key,value|
+  
+ def initialize(attr)
+      attr.each do |key,value|
          self.send(("#{key}"),value)
        end
-
-end
+ end
 
 
 end
