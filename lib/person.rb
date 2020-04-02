f438292d225747e6ed0121dd523e250ab8d06217
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-02 06:14:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   #your code here
-  attr_accessor :name,:birthday,:hair_color
+  attr_accessor :name,:birthday,:hair_color,:eye_color,:height
 
  def initialize(attr)
       attr.each do |key,value|
